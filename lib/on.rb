91a1a6ea97cfb233b4e28b97571fd36f34be085d
Peter Suschlik <ps@neopoly.de>
2012-11-21 10:34:45 +0100

    Add some docs.
    [ci skip]

--- lib/on.rb
+++ lib/on.rb
@@ -1,6 +1,27 @@
 require 'on/version'
 require 'set'
 
+# Dynamic callbacks for Ruby blocks.
+#
+# = Example
+#
+#  require 'on'
+#
+#  def tweet(message, &block)
+#    callback = On.new(:success, :failure, &block)
+#    callback.call :success
+#  rescue => e
+#    callback.call :failure, e.message
+#  end
+#
+#  tweet "hello world" do |callback|
+#    callback.on :success do
+#      # handle success
+#    end
+#    callback.on :failure do |error_message|
+#      # handle error message
+#    end
+#  end
 class On
   def initialize(*callbacks, &block)
     raise ArgumentError, "please provide at least one callback" if callbacks.empty?
@@ -9,12 +30,14 @@ class On
     @block      = block
   end
 
+  # Dispatch callback.
   def call(name, *args)
     validate_callback!(name)
     @callback = Callback.new(name, args)
     @block.call(self)
   end
 
+  # Handle a callback.
   def on(name, &block)
     validate_callback!(name)
     if @callback && @callback.name == name
@@ -24,7 +47,7 @@ class On
 
   Callback = Struct.new(:name, :args)
 
-  class InvalidCallback < StandardError
+  class InvalidCallback < StandardError # :nodoc:
     def initialize(name)
       super("Invalid callback #{name.inspect}")
     end
